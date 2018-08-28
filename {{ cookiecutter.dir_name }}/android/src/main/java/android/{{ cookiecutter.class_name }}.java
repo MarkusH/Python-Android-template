b1107c2065f7f885bcb581d52741d6e71495fffa
Markus Holtermann <info@markusholtermann.eu>
2018-08-28 10:38:47 +1000

    undo

--- {{ cookiecutter.dir_name }}/android/src/main/java/android/{{ cookiecutter.class_name }}.java
+++ {{ cookiecutter.dir_name }}/android/src/main/java/android/{{ cookiecutter.class_name }}.java
@@ -1,6 +1,6 @@
 package android;
 
-// import android.content.Intent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,12 +9,10 @@ public class {{ cookiecutter.class_name }} extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
 
-        // super.onCreate(savedInstanceState);
-
-        // Intent intent = new Intent(this, PythonActivity.class);
-        // startActivity(intent);
-        // finish();
+        Intent intent = new Intent(this, PythonActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
